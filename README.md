2abe10e53c39e7ad5b5d0026f2ec261536a3918a
Bálint Száray <6738847+Szbuli@users.noreply.github.com>
2024-10-23 21:00:26 +0200

    add pulse counter

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sensor controller
+
+## Main features
+
+15 configurable input
+supported voltage: 13.8V
+
+Input types:
+- alarm sensor (multiple signal levels for tamper detection)
+- pulse counter
+## Development tools
+- EAGLE- PCB design
+- STM32CubeIDE - Programming
--- firmware/f103_alarm_modul/Core/Src/freertos.c
+++ firmware/f103_alarm_modul/Core/Src/freertos.c
@@ -80,8 +80,7 @@ void StartTamperTask(void const *argument);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /* GetIdleTaskMemory prototype (linked to static allocation support) */
-void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
-		StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize);
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize);
 
 /* USER CODE BEGIN PREPOSTSLEEP */
 __weak void PreSleepProcessing(uint32_t *ulExpectedIdleTime) {
@@ -97,8 +96,7 @@ __weak void PostSleepProcessing(uint32_t *ulExpectedIdleTime) {
 static StaticTask_t xIdleTaskTCBBuffer;
 static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
 
-void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
-		StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize) {
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize) {
 	*ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
 	*ppxIdleTaskStackBuffer = &xIdleStack[0];
 	*pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
@@ -135,8 +133,7 @@ void MX_FREERTOS_Init(void) {
 
 	/* definition and creation of CAN_RECEIVE_QUEUE */
 	osMessageQDef(CAN_RECEIVE_QUEUE, 40, RECEIVED_CAN_OBJECT);
-	CAN_RECEIVE_QUEUEHandle = osMessageCreate(osMessageQ(CAN_RECEIVE_QUEUE),
-			NULL);
+	CAN_RECEIVE_QUEUEHandle = osMessageCreate(osMessageQ(CAN_RECEIVE_QUEUE), NULL);
 
 	/* USER CODE BEGIN RTOS_QUEUES */
 	/* add queues, ... */
@@ -285,8 +282,7 @@ void StartHeartbeatTask(void const *argument) {
 			HAL_GPIO_TogglePin(BUSY_LED_GPIO_Port, BUSY_LED_Pin);
 		}
 		if (i == 29) {
-			putCanMessageToQueue(ALARM_CONTROLLER_HEARTBEAT, &heartbeat, 1,
-					CAN_RTR_DATA);
+			putCanMessageToQueue(ALARM_CONTROLLER_HEARTBEAT, &heartbeat, 1, CAN_RTR_DATA);
 			i = 0;
 		}
 
